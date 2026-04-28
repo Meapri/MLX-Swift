ff02a7bdab498bb4a20395ca7d0a6c85020f0606
Prince Canuma <prince.gdt@gmail.com>
2026-04-29 01:02:59 +0200

    Refactor audio configuration: rename SoundConfig to AudioConfig a

--- mlx_vlm/models/nemotron_h_nano_omni/__init__.py
+++ mlx_vlm/models/nemotron_h_nano_omni/__init__.py
@@ -1,13 +1,13 @@
-from .config import ModelConfig, SoundConfig, TextConfig, VisionConfig
+from .config import AudioConfig, ModelConfig, TextConfig, VisionConfig
 from .language import LanguageModel
 from .nemotron_h_nano_omni import Model
 from .vision import VisionModel
 
 __all__ = [
+    "AudioConfig",
     "LanguageModel",
     "Model",
     "ModelConfig",
-    "SoundConfig",
     "TextConfig",
     "VisionConfig",
     "VisionModel",
