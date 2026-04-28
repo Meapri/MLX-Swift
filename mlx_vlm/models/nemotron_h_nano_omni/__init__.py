24185b09bce68affd317a6d3180dbf5f527c3180
Prince Canuma <prince.gdt@gmail.com>
2026-04-29 01:51:39 +0200

    add custom processor

--- mlx_vlm/models/nemotron_h_nano_omni/__init__.py
+++ mlx_vlm/models/nemotron_h_nano_omni/__init__.py
@@ -1,6 +1,7 @@
 from .config import AudioConfig, ModelConfig, TextConfig, VisionConfig
 from .language import LanguageModel
 from .nemotron_h_nano_omni import Model
+from .processing_nemotron_h_nano_omni import NemotronHNanoOmniProcessor
 from .vision import VisionModel
 
 __all__ = [
@@ -8,6 +9,7 @@ __all__ = [
     "LanguageModel",
     "Model",
     "ModelConfig",
+    "NemotronHNanoOmniProcessor",
     "TextConfig",
     "VisionConfig",
     "VisionModel",
